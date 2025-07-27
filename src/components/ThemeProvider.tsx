69d2a7ce42ec5797a2d0d7e0628477eadbe2733f
ld326 <63453228+ld326@users.noreply.github.com>
2025-07-27 23:36:28 +0800

    改暗色模式 ThemeProvider.tsx

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -7,11 +7,10 @@ import * as React from 'react';
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
-      attribute='class'
-      defaultTheme='system'
-      enableSystem
-      {...props}
-    >
+  attribute="class"
+  defaultTheme="dark"  // ← 默认暗色模式
+  enableSystem={false} // ← 禁用根据系统自动切换（可选）
+>
       {children}
     </NextThemesProvider>
   );
